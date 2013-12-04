2f836c32891abccbb7d942bcc4854694d1e02be0
Mare <bj080220@126.com>
2013-12-04 23:05:27 +0800

    Code Arrange
    Arrange Finished

---
+++ clearDataBase.sql
@@ -0,0 +1,10 @@
+use Note
+delete from comments
+delete from groups
+delete from messages
+delete from note_book
+delete from note_info
+delete from tags
+delete from teachers
+delete from user_detailed_info
+delete from user_login_info
